7d37e8e4b733c3e09831270ae21168ab32dcb039
prediatorr <rajeevkumarsingh5252@gmail.com>
2025-01-08 22:08:55 +0530

    home api

--- Server.js
+++ Server.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.json({ message: "Hello World" });
+  });
+
 app.use("/api/admin", adminRoutes);
 app.use("/api/client", clientRoutes);
 
